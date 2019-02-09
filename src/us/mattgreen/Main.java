62448ba3fb622f43a40fbdec6b96591b1c65fad6
jayer1 <jayer1@my.wctc.edu>
2019-02-09 13:54:42 -0600

    Problem with Collections.sort

---
+++ src/us/mattgreen/Main.java
@@ -0,0 +1,260 @@
+package us.mattgreen;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Collections;
+import java.text.DecimalFormat;
+
+public class Main {
+
+    private Scanner keyboard;
+    private Cookbook cookbook;
+    private Object Collections;
+
+    public Main() {
+        keyboard = new Scanner(System.in);
+        cookbook = new Cookbook();
+
+        FileInput indata = new FileInput("meals_data.csv");
+
+        String line;
+
+        System.out.println("Reading in meals information from file...");
+        while ((line = indata.fileReadLine()) != null) {
+            String[] fields = line.split(",");
+            cookbook.addElementWithStrings(fields[0], fields[1], fields[2]);
+        }
+
+        runMenu();
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    private void printMenu() {
+        System.out.println("");
+        System.out.println("Select Action");
+        System.out.println("1. List All Items");
+        System.out.println("2. List All Items by Meal");
+        System.out.println("3. Search by Meal Name");
+        System.out.println("4. Do Control Break");
+        System.out.println("5. Exit");
+        System.out.print("Please Enter your Choice: ");
+    }
+
+    private void runMenu() {
+        boolean userContinue = true;
+
+        while (userContinue) {
+            printMenu();
+
+            String ans = keyboard.nextLine();
+            switch (ans) {
+                case "1":
+                    cookbook.printAllMeals();
+                    break;
+                case "2":
+                    listByMealType();
+                    break;
+                case "3":
+                    searchByName();
+                    break;
+                case "4":
+                    doControlBreak();
+                    break;
+                case "5":
+                    userContinue = false;
+                    break;
+            }
+        }
+
+        System.out.println("Goodbye");
+        System.exit(0);
+    }
+
+    private void listByMealType() {
+        // Default value pre-selected in case
+        // something goes wrong w/user choice
+        MealType mealType = MealType.DINNER;
+
+        System.out.println("Which Meal Type");
+
+        // Generate the menu using the ordinal value of the enum
+        for (MealType m : MealType.values()) {
+            System.out.println((m.ordinal() + 1) + ". " + m.getMeal());
+        }
+
+        System.out.print("Please Enter your Choice: ");
+        String ans = keyboard.nextLine();
+
+        try {
+            int ansNum = Integer.parseInt(ans);
+            if (ansNum < MealType.values().length) {
+                mealType = MealType.values()[ansNum - 1];
+            }
+        } catch (NumberFormatException nfe) {
+            System.out.println("Invalid Meal Type " + ans + ", defaulted to " + mealType.getMeal() + ".");
+        }
+
+        cookbook.printMealsByType(mealType);
+    }
+
+    private void searchByName() {
+        keyboard.nextLine();
+        System.out.print("Please Enter Value: ");
+        String ans = keyboard.nextLine();
+        cookbook.printByNameSearch(ans);
+    }
+
+    private void doControlBreak() {
+
+        DecimalFormat df2 = new DecimalFormat(".##");
+        List<Meal> mealArrayList = new ArrayList<Meal>();
+        MealType mealType;
+        FileInput indata = new FileInput("meals_data.csv");
+        String line;
+        int i = 0;
+        int breakfastCount = 0;
+        int count = 0;
+        int nbr = 0;
+        double sum = 0;
+        Integer max = Integer.MIN_VALUE;
+
+        // Print the heading line
+        System.out.println("MealType\tTotal\tMean\tMin\tMax\tMedian");
+
+        String currentMealType = "BREAKFAST";
+
+        // Read the file and load to mealArrayList
+        while ((line = indata.fileReadLine()) != null) {
+            String[] fields = line.split(",");
+            //System.out.println("Meal" + fields[0]);
+            switch (fields[0]) {
+                case "Breakfast":
+                    mealType = MealType.BREAKFAST;
+                    break;
+                case "Lunch":
+                    mealType = MealType.LUNCH;
+                    break;
+                case "Dinner":
+                    mealType = MealType.DINNER;
+                    break;
+                case "Dessert":
+                    mealType = MealType.DESSERT;
+                    break;
+                default:
+                    mealType = MealType.DINNER;
+
+            }
+
+            // This adds the Meal object to the mealArrayList
+            mealArrayList.add(new Meal(mealType, fields[1], Integer.parseInt(fields[2])));
+            nbr = Integer.parseInt(fields[2]);
+        }
+
+        Collections.sort(mealArrayList);
+        // This instantiates the arraylist which grabs calories for each mealtype
+        ArrayList<Integer> breakfast = new ArrayList<>();
+
+        //System.out.println("Current meal type is " + currentMealType.toUpperCase());
+        for (Meal str : mealArrayList) {
+            //System.out.println("Record meal type is " + str.getMealType());
+            // If currentMealType value is same as next record, add the calories to arraylist
+            if (currentMealType.toUpperCase().equals(str.getMealType().toString())) {
+
+                breakfast.add(str.getCalories());
+
+            } else {
+                // If the currentMealType value is different than next record, do the calcs, display output and reset the arraylist
+                //System.out.println("Meal type is different - performing calculations");
+
+                // Find max calorie value, count and total in arraylist
+                for (Integer number : breakfast) {
+                    sum = sum + number;
+                    count++;
+                    if (number > max) {
+                        max = number;
+                    }
+                }
+
+                // Find min calorie value in arraylist
+                Integer min = max;
+                for (Integer number : breakfast) {
+                    if (number < min) {
+                        min = number;
+                    }
+                }
+
+                // Find the mean value
+                double mean = sum / count;
+
+                // Print the output
+                System.out.println(currentMealType + "  \t" + df2.format(sum) + "\t" + df2.format(mean) + "\t" + min + "\t" + max);
+
+                //System.out.println("Clearing array and Updating Meal type");
+                breakfast.clear();
+                currentMealType = str.getMealType().toString();
+                sum = 0;
+                count = 0;
+                breakfast.add(str.getCalories());
+            }
+
+            /*if (str.getMealType() == MealType.BREAKFAST) {
+             //System.out.println("My arraylist of meal objects (calories)" + str.getCalories());
+
+             breakfast.add(str.getCalories());
+             }
+
+             if (str.getMealType() == MealType.LUNCH) {
+             //System.out.println("My arraylist of meal objects (calories)" + str.getCalories());
+
+             lunch.add(str.getCalories());
+             }*/
+        }
+
+        //Collections.sort(breakfast);
+        /*
+         System.out.println("Calories created for breakfast ");
+         for (int calorieElementInBreakfast : breakfast) {
+         System.out.println(calorieElementInBreakfast);
+
+         }
+
+         System.out.println("Calories created for lunch ");
+         for (int calorieElementInLunch : lunch) {
+         System.out.println(calorieElementInLunch);
+         }
+
+         System.out.println("Breakfast size" + breakfast.size());
+         System.out.println("Lunch size" + lunch.size());
+         */
+        //System.out.println("Max is " + max);
+        //System.out.println("Min is " + min);
+        //System.out.println("Breakfast mean" + sum / count);
+        //for (int elem : breakfastArray) {
+        //    System.out.println("Calories " + elem);//do whatever with the element
+        //}
+
+        /*System.out.println("");
+         for (Meal item : myList) {
+         if (item != null) {
+         System.out.println(item);
+         }
+         }*/
+        //iterate through the list - look at the name (breakfast, lunch) and grab the calories
+        //when name changes, do math on list of calories of group taken
+        //cookbook.printAllMeals();
+        /*ArrayList<Meal> myList = new ArrayList<Meal>();
+         FileInput indata = new FileInput("meals_data.csv");
+
+         String line;
+
+         System.out.println("Reading in meals information from file...");
+         while ((line = indata.fileReadLine()) != null) {
+         String[] fields = line.split(",");
+         myList.add(fields[0], fields[1], fields[2]);
+         }*/
+    }
+}
---
+++ src/us/mattgreen/MealType.java
@@ -0,0 +1,16 @@
+package us.mattgreen;
+
+public enum MealType {
+
+    BREAKFAST("Breakfast"), DESSERT("Dessert"), DINNER("Dinner"), LUNCH("Lunch");
+
+    private String meal;
+
+    MealType(String meal) {
+        this.meal = meal;
+    }
+
+    public String getMeal() {
+        return meal;
+    }
+}
